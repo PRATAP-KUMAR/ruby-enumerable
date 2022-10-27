89630de7a3b83f5f34412718b2c511cdafb49c2b
PraisesPJMT <praisesmusa@outlook.com>
2022-10-27 10:24:30 +0100

    Add `#each` method for the `MyList` class - Also added tests for `#all?` method - Also added tests for `#any?` method

--- MyList.rb
+++ MyList.rb
@@ -1,11 +1,26 @@
+require_relative 'MyEnumerable'
 class MyList
+    include MyEnumerable
     def initialize (*array)
         @list = array
     end
 
-    def each(my_list)
-        @my_list.each {|e| puts e}
+    def each(&my_list)
+        # @my_list.each {|e| puts e}
+        @list.each(&my_list)
     end
 end
 
 list = MyList.new(1, 2, 3, 4)
+
+# Test #all?
+puts list.all? {|e| e < 5}
+# => true
+puts list.all? {|e| e > 5}
+# => false
+
+# Test #any?
+puts list.any? {|e| e == 3}
+# => true
+puts list.any? {|e| e == 5}
+# => false
